0f9ced86c6d5753d5226faf5e0fbf577e6bbb2d1
davnpsh <git@davnpsh.dev>
2024-07-14 11:43:07 -0500

    Listen on any interface

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,12 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const host = "127.0.0.1";
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript with Express!");
 });
 
-app.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
