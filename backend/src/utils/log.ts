64c5ae4d988fa114bae817d8bdf817096d250d3a
davnpsh <git@davnpsh.dev>
2024-07-15 06:54:43 -0500

    Feature: Log function added

---
+++ backend/src/utils/log.ts
@@ -0,0 +1,29 @@
+export enum LogType {
+  error = "ERROR",
+  info = "INFO",
+}
+
+/**
+ * Logs a message through the server console.
+ * @param {LogType} type - Type of the message.
+ * @param {string} message - Main message to display.
+ * @param {string[]} opt_msgs - Optional arguments to display along the main message.
+ */
+export default function (
+  type: LogType,
+  message: string,
+  ...opt_msgs: string[]
+) {
+  if (opt_msgs.length > 0) {
+    message += " " + opt_msgs.join(" ");
+  }
+
+  switch (type) {
+    case LogType.error:
+      console.error(`[ backend ][ ${LogType.error} ] ${message}`);
+      break;
+    case LogType.info:
+      console.log(`[ backend ][ ${LogType.info} ] ${message}`);
+      break;
+  }
+}
