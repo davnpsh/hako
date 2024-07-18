34d54e9819ffb8fcd99abbae579ff2f1fae9aa90
FiboDev <juanpadilla@uninorte.edu.co>
2024-07-17 19:18:44 -0500

    docker obj updated

--- backend/src/docker/index.ts
+++ backend/src/docker/index.ts
@@ -1,51 +1,67 @@
 import log, { LogType } from "../utils/log";
+import { ErrorType } from "../enums/error";
+import { Container } from "./interfaces/container";
+import { Docker } from "./interfaces/docker";
+
 import { exec } from "child_process";
-
-interface Container {
-  id: string;
-  name: string;
-  image: string;
-  compose_project: string;
-}
-
-interface Docker {
-  /**
-   * Returns a list of containers.
-   * @returns {object}
-   */
-  containers: () => Container[];
-}
+import http from "http";
 
 /**
  * Returns a Docker object with methods to interact with Docker containers.
  * @returns {Docker} Docker object.
  */
-export default function (): Docker {
+export default function (socketPath: string): Docker {
   const obj: Docker = {
-    containers: function (): Container[] {
-      var containers: Container[] = [];
+    socketPath: socketPath,
+    isRunning: function (): Promise<boolean> {
+      return new Promise((resolve) => {
+        const options = {
+          socketPath: socketPath,
+          path: "/_ping",
+          method: "GET",
+        };
 
-      const cmd: string = `docker container ls --all --no-trunc --format '{"id": "{{.ID}}", "name": "{{.Names}}", "image": "{{.Image}}", "compose_project": "{{.Label "com.docker.compose.project"}}"}'`;
-      exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-          log(LogType.error, "Couldn't retrieve list of containers:\n", stderr);
-          return;
-        }
+        const req = http.request(options, (res) => {
+          if (res.statusCode === 200) {
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        });
 
-        try {
-          containers = stdout
-            .trim()
-            .split("\n")
-            .map((container) => JSON.parse(container));
+        req.on("error", (error) => {
+          console.error(ErrorType.DOCKER_SOCKET_NOT_RUNNING, error);
+          resolve(false);
+        });
 
-          log(LogType.info, "Retrieved list of containers.");
-          console.log(containers);
-        } catch (exception: any) {
-          log(LogType.info, "Couldn't retrieve list of containers:", exception);
-        }
+        req.end();
       });
+    },
+    containers: function (): Promise<Container[] | ErrorType> {
+      return new Promise((resolve) => {
+        const cmd: string = `docker container ls --all --no-trunc --format '{"id": "{{.ID}}", "name": "{{.Names}}", "image": "{{.Image}}", "compose_project": "{{.Label "com.docker.compose.project"}}"}'`;
+        exec(cmd, (error, stdout, stderr) => {
+          if (error) {
+            log(LogType.error, ErrorType.CONTAINERS_LOOKUP_FAILED, stderr);
+            resolve(ErrorType.CONTAINERS_LOOKUP_FAILED);
+            return;
+          }
 
-      return containers;
+          try {
+            let containers: Container[] = stdout
+              .trim()
+              .split("\n")
+              .map((container) => JSON.parse(container));
+
+            log(LogType.info, "Retrieved list of containers.");
+            console.log(containers);
+            resolve(containers);
+          } catch (exception: any) {
+            log(LogType.error, ErrorType.CONTAINERS_LOOKUP_FAILED, exception);
+            resolve(ErrorType.CONTAINERS_LOOKUP_FAILED);
+          }
+        });
+      });
     },
   };
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,31 @@
 import log, { LogType } from "./utils/log";
 import express, { Request, Response } from "express";
+import { ErrorType } from "./enums/error";
+import { sendError } from "./utils/error";
+import { Container } from "./docker/interfaces/container";
 import Docker from "./docker";
 
 const app = express();
 const port = 3000;
 
 // APIs
-const docker = Docker();
+const docker = Docker("/var/run/docker.sock");
 
-app.get("/docker/containers", (req: Request, res: Response) => {
-  res.send(docker.containers());
+// check if Docker socket
+docker.isRunning().then((isRunning) => {
+  if (isRunning === false) {
+    log(LogType.error, ErrorType.DOCKER_SOCKET_NOT_RUNNING);
+  }
+});
+
+app.get("/docker/containers", async (req: Request, res: Response) => {
+  let containers: Container[] | ErrorType = await docker.containers();
+
+  if (Array.isArray(containers) === false) {
+    sendError(res, 500, containers as ErrorType);
+    return;
+  }
+  res.json(containers);
 });
 
 app.listen(port, () => {
