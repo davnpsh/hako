b35097458ace62ab9d07328f861d9331f201a777
davnpsh <git@davnpsh.dev>
2024-07-19 09:38:06 -0500

    Clarified message

--- backend/src/enums/info.ts
+++ backend/src/enums/info.ts
@@ -1,5 +1,5 @@
 export enum InfoMessage {
   DOCKER_SOCKET_RUNNING = "Docker socket is running.",
   DOCKER_SOCKET_INFORMATION_LOOKUP = "Fetched Docker socket information.",
-  DOCKER_CONTAINERS_LOOKUP = "Fetched Docker containers information.",
+  DOCKER_CONTAINERS_LOOKUP = "Fetched Docker containers list.",
 }
