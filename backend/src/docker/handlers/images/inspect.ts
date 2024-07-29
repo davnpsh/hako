9baa47314aaf2e7e1e5cc2646c5940f6a5a07866
davnpsh <git@davnpsh.dev>
2024-07-29 10:40:33 -0500

    change: var name

--- backend/src/docker/handlers/images/inspect.ts
+++ backend/src/docker/handlers/images/inspect.ts
@@ -19,12 +19,14 @@ export default async function (socket: Socket, id: string): Promise<Image> {
   // Date comes in another format when inspecting individual images.
   // For consistency, convert created date to UNIX timestamp:
   const created_date = new Date(response.data.Created);
-  const unix_timestamp = Math.floor(created_date.getTime() / 1000).toString();
+  const created_date_unix = Math.floor(
+    created_date.getTime() / 1000,
+  ).toString();
 
   const image: Image = {
     id: response.data.Id.replace(/^sha256:/, ""), // Get clean ID
     tags: response.data.RepoTags,
-    created: unix_timestamp,
+    created: created_date_unix,
     size: response.data.Size,
   };
 
